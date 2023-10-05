b8ce4ec4d9b993b102c0a6c17a9929d067d45abf
Boclair Temgoua <temgoua2000@gmail.com>
2023-10-05 22:19:55 +0200

    fix(bug): fix bug

--- components/event/public-events.tsx
+++ components/event/public-events.tsx
@@ -9,10 +9,10 @@ import ListPublicPosts from "./list-public-events";
 import { LoadingFile } from "../ui/loading-file";
 
 type Props = {
-  userId: string;
+  organizationId: string;
 };
 
-const PublicPosts: React.FC<Props> = ({ userId }) => {
+const PublicPosts: React.FC<Props> = ({ organizationId }) => {
   const { ref, inView } = useInView();
 
   const {
@@ -25,7 +25,7 @@ const PublicPosts: React.FC<Props> = ({ userId }) => {
   } = GetInfinitePostsAPI({
     take: 10,
     sort: "DESC",
-    userId: userId,
+    organizationId: organizationId,
     status: 'ACTIVE',
     typeIds: ['ARTICLE', 'AUDIO', 'VIDEO'],
     queryKey: ['posts', "infinite"]
