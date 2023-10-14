1f6f919b1c002f39ac77b9d9f3adda0e5a79b0bb
Boclair Temgoua <temgoua2000@gmail.com>
2023-10-14 07:16:55 +0200

    fix(module): fix bug

--- components/layout-dashboard/vertical-nav-dashboard.tsx
+++ components/layout-dashboard/vertical-nav-dashboard.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-icons/bi";
 import { BsShop } from "react-icons/bs";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 export type NavbarProps = {
   title: string;
@@ -54,6 +55,7 @@ interface Props {
 
 const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
   const pathname = usePathname();
+  const router = useRouter();
   const [navigationItems] = useState<NavbarProps[]>([
     {
       title: "Home",
@@ -66,6 +68,10 @@ const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
   const [supportItems] = useState<NavbarProps[]>(SUPPORT_ITEMS);
   const [settingItems] = useState<NavbarProps[]>(SETTINGS_ITEMS);
 
+  const logoutUser = () => {
+    localStorage.removeItem(String(process.env.NEXT_PUBLIC_BASE_NAME_TOKEN))
+    router.push(`${`/`}`);
+  }
   return (
     <>
       <div className="flex flex-col justify-between flex-1 h-full px-4 overflow-x-scroll">
@@ -78,11 +84,10 @@ const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
                   key={index}
                   href={`${item.href}`}
                   title={item?.title}
-                  className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${
-                    isActive
-                      ? `text-white bg-${user?.profile?.color}-600`
-                      : "hover:bg-gray-200 text-gray-900"
-                  } `}
+                  className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${isActive
+                    ? `text-white bg-${user?.profile?.color}-600`
+                    : "hover:bg-gray-200 text-gray-900"
+                    } `}
                 >
                   {item?.icon}
 
@@ -105,11 +110,10 @@ const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
                     key={index}
                     href={`${item.href}`}
                     title={item?.title}
-                    className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${
-                      isActive
-                        ? `text-white bg-${user?.profile?.color}-600`
-                        : "hover:bg-gray-200 text-gray-900"
-                    } `}
+                    className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${isActive
+                      ? `text-white bg-${user?.profile?.color}-600`
+                      : "hover:bg-gray-200 text-gray-900"
+                      } `}
                   >
                     {item?.icon}
 
@@ -132,11 +136,10 @@ const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
                     key={index}
                     href={`${item.href}`}
                     title={item?.title}
-                    className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${
-                      isActive
-                        ? `text-white bg-${user?.profile?.color}-600`
-                        : "hover:bg-gray-200 text-gray-900"
-                    } `}
+                    className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${isActive
+                      ? `text-white bg-${user?.profile?.color}-600`
+                      : "hover:bg-gray-200 text-gray-900"
+                      } `}
                   >
                     {item?.icon}
 
@@ -159,11 +162,10 @@ const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
                     key={index}
                     href={`${item.href}`}
                     title={item?.title}
-                    className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${
-                      isActive
-                        ? `text-white bg-${user?.profile?.color}-600`
-                        : "hover:bg-gray-200 text-gray-900"
-                    } `}
+                    className={`flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 group rounded-lg ${isActive
+                      ? `text-white bg-${user?.profile?.color}-600`
+                      : "hover:bg-gray-200 text-gray-900"
+                      } `}
                   >
                     {item?.icon}
 
@@ -173,9 +175,11 @@ const VerticalNavDashboard: React.FC<Props> = ({ user }) => {
               })}
 
               <a
-                href="#"
+                href={void (0)}
                 title=""
-                className="flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 text-gray-900 rounded-lg hover:bg-gray-200 group"
+                onClick={() => logoutUser()}
+
+                className="flex items-center px-4 py-2 text-sm font-medium transition-all duration-200 text-gray-900 rounded-lg hover:bg-gray-200 group cursor-pointer"
               >
                 <svg
                   className="flex-shrink-0 w-5 h-5 mr-4"
